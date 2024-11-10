ec2223d95de957f2aa133ca231321e2ff8437cba
wbfm1 <martinusss111@gmail.com>
2024-11-10 21:07:18 +0100

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+// Smooth Scroll for Contact Button
+document.querySelector('.btn').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('#contact').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+// Form Submission Alert
 document.querySelector("form").addEventListener("submit", function(event) {
-  event.preventDefault();
-  alert("Thank you for your message!");
+    event.preventDefault();
+    alert("Thank you for your message! We will get back to you soon.");
+    event.target.reset();
 });
